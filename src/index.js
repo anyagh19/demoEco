67e515a2a4ba058223cf18664f87fdff4c185e58
anyagh19 <aniketmarkad1509@gmail.com>
2025-07-24 20:26:45 +0530

    port

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import dotenv from 'dotenv';
 import { connect } from './db/index.js';
 
 
-const PORT = 3000
-;
+const PORT = process.env.PORT || 3000;
+
 
 connect()
     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`server is ${PORT}`);
-            
-        })
+        app.listen(PORT, '0.0.0.0', () => {
+            console.log(`Server is running on port ${PORT}`);
+          });
     })
     .catch((err) => {
         console.log("connect errror" , err)
